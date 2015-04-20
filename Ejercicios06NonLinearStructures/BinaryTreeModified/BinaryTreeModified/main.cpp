be373313889b3d282cee7ef1a18f8eb1a9b28a4d
Gerardo Teruel <gerardogtn@gmail.com>
2015-04-20 11:33:21 -0500

    Updated BinaryTreeModified

---
+++ Ejercicios06NonLinearStructures/BinaryTreeModified/BinaryTreeModified/main.cpp
@@ -0,0 +1,36 @@
+//
+//  main.cpp
+//  BinaryTreeModified
+//
+//  Created by Gerardo Teruel on 4/13/15.
+//  Copyright (c) 2015 Gerardo Teruel. All rights reserved.
+//
+
+#include <iostream>
+#include "BinaryTree.h"
+
+using namespace vcn; 
+
+int main(int argc, const char * argv[]) {
+    BinaryTree<int> * numeros = new BinaryTree<int>();
+    
+    BNode<int> * uno = new BNode<int>(1);
+    numeros->insert(nullptr, uno);
+    
+    
+    BNode<int> * dos = new BNode<int>(2);
+    numeros->insert(uno, dos);
+    
+    numeros->insert(nullptr, 0);
+    
+    BNode<int> * tres = new BNode<int>(3);
+    numeros->insert(uno, tres);
+    
+    BNode<int> * cuatro = new BNode<int>(4);
+    numeros->insert(dos, cuatro);
+    
+    numeros->preOrder();
+    
+    delete numeros;
+
+}
