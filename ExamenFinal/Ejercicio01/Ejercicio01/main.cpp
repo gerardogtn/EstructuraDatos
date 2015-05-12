e640577b67d0c9a027c3e18410952a1b2c54a6e7
Gerardo Teruel <gerardogtn@gmail.com>
2015-05-12 12:36:38 -0500

    Final version ejercicio01

--- ExamenFinal/Ejercicio01/Ejercicio01/main.cpp
+++ ExamenFinal/Ejercicio01/Ejercicio01/main.cpp
@@ -7,9 +7,43 @@
 //
 
 #include <iostream>
+#include "Graph.h"
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
-    return 0;
+    Graph<char, int> mapa("Guía Roji");
+    
+    /* Crear vértices */
+    Vertex<char, int> * A = new Vertex<char, int>('A');
+    Vertex<char, int> * B = new Vertex<char, int>('B');
+    Vertex<char, int> * C = new Vertex<char, int>('C');
+    Vertex<char, int> * D = new Vertex<char, int>('D');
+    Vertex<char, int> * E = new Vertex<char, int>('E');
+    
+    /* Adicionar vértices al grafo */
+    mapa.addVertex(A);
+    mapa.addVertex(B);
+    
+    mapa.addEdge(A, B, 30);
+    mapa.addEdge(B, A, 30);
+    std::cout << (mapa.isComplete()? "Works" : "Doesn't work") << std::endl;
+    
+    mapa.addVertex(C);
+    mapa.addVertex(D);
+    mapa.addVertex(E);
+    
+    /* Adicionar aristas */
+    
+    mapa.addEdge(A, E, 100);
+    mapa.addEdge(B, D, 75);
+    mapa.addEdge(C, D, 20);
+    mapa.addEdge(D, B, 60);
+    mapa.addEdge(E, C, 15);
+    
+    /* Eliminar una arista */;
+    mapa.removeEdge(A, B, 30);
+    
+    /* Mostrar el grafo */
+    std::cout << mapa << std::endl;
+    
+    std::cout << (mapa.isComplete()? "Doesn'tWork" : "Works") << std::endl;
 }
